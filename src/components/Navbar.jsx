74983392cf048a30fdb5305d561fe23bb79df004
Walter Mazzariol <mazzariolwalter@gmail.com>
2026-09-08 21:18:05 +0200

    add contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import Logo from "../assets/data/images/logo.svg"
 
 function NavBar(props) {
   return (
-    <Navbar className="navbar navbar-expand-lg navbar-light fixed-top py-0" id="mainNav" expand="lg" collapseOnSelect={true}>
+    <Navbar className="navbar navbar-expand-xl navbar-light fixed-top py-0" id="mainNav" expand="xl" collapseOnSelect={true}>
       <div className="container">
         <Navbar.Toggle aria-controls="navbarResponsive" />
 
         <Link className="navbar-brand" to='/'><img src={Logo} alt="logo Guarapo Media" /></Link>
 
-        <Link className="button button--primary d-sm-block d-md-block d-lg-none" to={props.nav.cta_link}>{props.nav.cta_button}</Link>
+        <Link className="button button--primary d-sm-block d-md-block d-xl-none" to={props.nav.cta_link}>{props.nav.cta_button}</Link>
         <Navbar.Collapse id="navbarResponsive">
           <Nav className="navbar-nav ms-auto my-2 my-lg-0">
             <div className="navbar-nav"
@@ -25,7 +25,7 @@ function NavBar(props) {
             </div>
           </Nav>
         </Navbar.Collapse>
-        <Link className="button button--primary d-none d-lg-block" to={props.nav.cta_link}>{props.nav.cta_button}</Link>
+        <Link className="button button--primary d-none d-xl-block" to={props.nav.cta_link}>{props.nav.cta_button}</Link>
       </div>
     </Navbar>
   );
